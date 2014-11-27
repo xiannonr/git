fbd3c2c05854541e771f2176fad32990bf283e8e
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-11-27 18:21:06 +0100

    WIP: Add a test for non-boolean receive.fsckobjects values
    TODO!!! This only *happens* to work because the fsck machinery is
    ill-prepared to handle warnings. It is based on the concept that we can
    simply error out when there is so much as even a warning, but what we want
    to do is to try to figure things out and continue even after detecting
    that, say, an email is missing or invalid.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5504-fetch-receive-strict.sh
+++ t/t5504-fetch-receive-strict.sh
@@ -115,4 +115,23 @@ test_expect_success 'push with transfer.fsckobjects' '
 	test_cmp exp act
 '
 
+cat >bogus-commit << EOF
+tree 4b825dc642cb6eb9a060e54bf8d69288fbee4904
+author Bugs Bunny <bugs@bun.ni> 1234567890 +0000
+committer Bugs Bunny 1234567890 +0000
+
+This commit object intentionally broken
+EOF
+
+test_expect_success 'push with receive.fsckobjects = -missing_email' '
+	commit="$(git hash-object -t commit -w --stdin < bogus-commit)" &&
+	git push . $commit:refs/heads/bogus &&
+	rm -rf dst &&
+	git init dst &&
+	git --git-dir=dst/.git config receive.fsckobjects true &&
+	test_must_fail git push --porcelain dst bogus:refs/heads/bogus &&
+	git --git-dir=dst/.git config receive.fsckobjects -missing_email &&
+	git push --porcelain dst bogus:refs/heads/bogus
+'
+
 test_done
