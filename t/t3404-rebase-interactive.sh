2751d249ba6315f0139ba8fd6aa6ff23c32c9e69
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-06-17 09:29:39 +0200

    rebase -i: do not leave a CHERRY_PICK_HEAD file behind
    When skipping commits whose changes were already applied via `git rebase
    --continue`, we need to clean up said file explicitly.
    
    The same is not true for `git rebase --skip` because that will execute
    `git reset --hard` as part of the "skip" handling in git-rebase.sh, even
    before git-rebase--interactive.sh is called.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t3404-rebase-interactive.sh
+++ t/t3404-rebase-interactive.sh
@@ -1102,7 +1102,7 @@ test_expect_success 'rebase -i commits that overwrite untracked files (no ff)' '
 	test $(git cat-file commit HEAD | sed -ne \$p) = I
 '
 
-test_expect_failure 'rebase --continue removes CHERRY_PICK_HEAD' '
+test_expect_success 'rebase --continue removes CHERRY_PICK_HEAD' '
 	git checkout -b commit-to-skip &&
 	for double in X 3 1
 	do
