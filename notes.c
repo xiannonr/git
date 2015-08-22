2d7381810773124ccf8fbe86c6e3ffc367952915
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-08-22 15:02:05 +0000

    Fix `git rev-list --show-notes HEAD` when there are no notes
    The `format_display_notes()` function clearly assumes that the data
    structure holding the notes has been initialized already, i.e. that the
    `display_notes_trees` variable is no longer `NULL`.
    
    However, when there are no notes whatsoever, this variable is still
    `NULL`, even after initialization.
    
    So let's be graceful and just return if that data structure is `NULL`.
    
    Reported in https://github.com/msysgit/git/issues/363.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- notes.c
+++ notes.c
@@ -1266,7 +1266,10 @@ void format_display_notes(const unsigned char *object_sha1,
 			  struct strbuf *sb, const char *output_encoding, int raw)
 {
 	int i;
-	assert(display_notes_trees);
+
+	if (!display_notes_trees)
+		return;
+
 	for (i = 0; display_notes_trees[i]; i++)
 		format_note(display_notes_trees[i], object_sha1, sb,
 			    output_encoding, raw);
