0904370a1a99f3efd88bd7b91619c3f1357ed1da
Stefan Beller <sbeller@google.com>
2015-09-22 18:45:20 -0700

    xread: poll on non blocking fds
    From the man page:
    EAGAIN The file descriptor fd refers to a file other than a socket
           and has been marked nonblocking (O_NONBLOCK), and the read
           would block.
    
    EAGAIN or EWOULDBLOCK
           The file descriptor fd refers to a socket and has been marked
           nonblocking (O_NONBLOCK), and the read would block.  POSIX.1-2001
           allows either error to be returned for this case, and does not
           require these constants to have the same value, so a portable
           application should check for both possibilities.
    
    If we get an EAGAIN or EWOULDBLOCK the fd must have set O_NONBLOCK.
    As the intent of xread is to read as much as possible either until the
    fd is EOF or an actual error occurs, we can ease the feeder of the fd
    by not spinning the whole time, but rather wait for it politely by not
    busy waiting.
    
    We should not care if the call to poll failed, as we're in an infinite
    loop and can only get out with the correct read().
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wrapper.c
+++ wrapper.c
@@ -201,8 +201,17 @@ ssize_t xread(int fd, void *buf, size_t len)
 	    len = MAX_IO_SIZE;
 	while (1) {
 		nr = read(fd, buf, len);
-		if ((nr < 0) && (errno == EAGAIN || errno == EINTR))
-			continue;
+		if (nr < 0) {
+			if (errno == EINTR)
+				continue;
+			if (errno == EAGAIN || errno == EWOULDBLOCK) {
+				struct pollfd pfd;
+				pfd.events = POLLIN;
+				pfd.fd = fd;
+				/* We deliberately ignore the return value */
+				poll(&pfd, 1, -1);
+			}
+		}
 		return nr;
 	}
 }
