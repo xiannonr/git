80062be27256704617280fdb479176e0d2afb4d4
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-03-09 15:32:18 +0100

    sequencer: lib'ify create_seq_dir()
    To be truly useful, the sequencer should never die() but always return
    an error.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -839,8 +839,8 @@ static int create_seq_dir(void)
 		return -1;
 	}
 	else if (mkdir(git_path_seq_dir(), 0777) < 0)
-		die_errno(_("Could not create sequencer directory %s"),
-			  git_path_seq_dir());
+		return error(_("Could not create sequencer directory %s (%s)"),
+			  git_path_seq_dir(), strerror(errno));
 	return 0;
 }
 
