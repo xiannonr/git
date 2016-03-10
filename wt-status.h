f693f6719c44c033fdaab5a6adb3b4b415d0f454
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-03-10 15:03:03 +0100

    Export also the has_un{staged,committed}_changed() functions
    They will be used in the upcoming rebase helper.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- wt-status.h
+++ wt-status.h
@@ -115,6 +115,8 @@ void status_printf_ln(struct wt_status *s, const char *color, const char *fmt, .
 __attribute__((format (printf, 3, 4)))
 void status_printf(struct wt_status *s, const char *color, const char *fmt, ...);
 
+int has_unstaged_changes(void);
+int has_uncommitted_changes(void);
 int require_clean_work_tree(const char *action, const char *hint, int gently);
 
 #endif /* STATUS_H */
