fb9776070670c5216143f30dd518dc1bb2b07ffb
Christian Couder <christian.couder@gmail.com>
2016-06-27 20:24:21 +0200

    write_or_die: use warning() instead of fprintf(stderr, ...)
    In write_or_whine_pipe() and write_or_whine() when write_in_full()
    returns an error, let's print the errno related error message using
    warning() instead of fprintf(stderr, ...).
    
    This makes it possible to change the way it is handled by changing
    the current warn routine in usage.c.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- write_or_die.c
+++ write_or_die.c
@@ -87,8 +87,7 @@ int write_or_whine_pipe(int fd, const void *buf, size_t count, const char *msg)
 {
 	if (write_in_full(fd, buf, count) < 0) {
 		check_pipe(errno);
-		fprintf(stderr, "%s: write error (%s)\n",
-			msg, strerror(errno));
+		warning("%s: write error (%s)\n", msg, strerror(errno));
 		return 0;
 	}
 
@@ -98,8 +97,7 @@ int write_or_whine_pipe(int fd, const void *buf, size_t count, const char *msg)
 int write_or_whine(int fd, const void *buf, size_t count, const char *msg)
 {
 	if (write_in_full(fd, buf, count) < 0) {
-		fprintf(stderr, "%s: write error (%s)\n",
-			msg, strerror(errno));
+		warning("%s: write error (%s)\n", msg, strerror(errno));
 		return 0;
 	}
 
