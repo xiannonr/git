f5ef009d2be6bf0137b33ac9ce85ff0ce03a48e8
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-07-09 13:39:10 +0200

    fsck_walk(): optionally name objects on the go
    If fsck_options->name_objects is initialized, and if it already has
    name(s) for the object(s) that are to be the starting point(s) for
    fsck_walk(), then that function will now add names for the objects
    that were walked.
    
    This will be highly useful for teaching git-fsck to identify root causes
    for broken links, which is the task for the next patch in this series.
    
    Note that this patch opts for decorating the objects with plain strings
    instead of full-blown structs (à la `struct rev_name` in the code of
    the `git name-rev` command), for several reasons:
    
    - the code is much simpler than if it had to work with structs that
      describe arbitrarily long names such as "master~14^2~5:builtin/am.c",
    
    - the string processing is actually quite light-weight compared to the
      rest of fsck's operation,
    
    - the caller of fsck_walk() is expected to provide names for the
      starting points, and using plain and simple strings is just the
      easiest way to do that.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- fsck.c
+++ fsck.c
@@ -9,6 +9,7 @@
 #include "refs.h"
 #include "utf8.h"
 #include "sha1-array.h"
+#include "decorate.h"
 
 #define FSCK_FATAL -1
 #define FSCK_INFO -2
@@ -297,18 +298,50 @@ static int report(struct fsck_options *options, struct object *object,
 	return result;
 }
 
+static char *get_object_name(struct fsck_options *options, struct object *obj)
+{
+	return options->object_names ?
+		lookup_decoration(options->object_names, obj) : NULL;
+}
+
+static void put_object_name(struct fsck_options *options, struct object *obj,
+	const char *fmt, ...)
+{
+	va_list ap;
+	char *existing = lookup_decoration(options->object_names, obj);
+	struct strbuf buf = STRBUF_INIT;
+
+	if (existing)
+		return;
+	va_start(ap, fmt);
+	strbuf_vaddf(&buf, fmt, ap);
+	add_decoration(options->object_names, obj, strbuf_detach(&buf, NULL));
+	va_end(ap);
+}
+
 static int fsck_walk_tree(struct tree *tree, void *data, struct fsck_options *options)
 {
 	struct tree_desc desc;
 	struct name_entry entry;
 	int res = 0;
+	const char *name;
 
 	if (parse_tree(tree))
 		return -1;
 
+	name = get_object_name(options, &tree->object);
 	init_tree_desc(&desc, tree->buffer, tree->size);
 	while (tree_entry(&desc, &entry)) {
 		int result;
+
+		if (name) {
+			struct object *obj = parse_object(entry.oid->hash);
+
+			if (obj)
+				put_object_name(options, obj, "%s%s%s", name,
+					entry.path,
+					S_ISDIR(entry.mode) ? "/" : "");
+		}
 
 		if (S_ISGITLINK(entry.mode))
 			continue;
@@ -330,12 +363,18 @@ static int fsck_walk_tree(struct tree *tree, void *data, struct fsck_options *op
 
 static int fsck_walk_commit(struct commit *commit, void *data, struct fsck_options *options)
 {
+	int counter = 0, generation = 0, name_prefix_len = 0;
 	struct commit_list *parents;
 	int res;
 	int result;
+	const char *name;
 
 	if (parse_commit(commit))
 		return -1;
+
+	name = get_object_name(options, &commit->object);
+	if (name)
+		put_object_name(options, &commit->tree->object, "%s:", name);
 
 	result = options->walk((struct object *)commit->tree, OBJ_TREE, data, options);
 	if (result < 0)
@@ -343,7 +382,36 @@ static int fsck_walk_commit(struct commit *commit, void *data, struct fsck_optio
 	res = result;
 
 	parents = commit->parents;
+	if (name && parents) {
+		int len = strlen(name), power;
+
+		if (len && name[len - 1] == '^') {
+			generation = 1;
+			name_prefix_len = len - 1;
+		}
+		else { /* parse ~<generation> suffix */
+			for (generation = 0, power = 1;
+			     len && isdigit(name[len - 1]);
+			     power *= 10)
+				generation += power * (name[--len] - '0');
+			if (power > 1 && len && name[len - 1] == '~')
+				name_prefix_len = len - 1;
+		}
+	}
+
 	while (parents) {
+		if (name) {
+			struct object *obj = &parents->item->object;
+
+			if (++counter > 1)
+				put_object_name(options, obj, "%s^%d",
+					name, counter);
+			else if (generation > 0)
+				put_object_name(options, obj, "%.*s~%d",
+					name_prefix_len, name, generation + 1);
+			else
+				put_object_name(options, obj, "%s^", name);
+		}
 		result = options->walk((struct object *)parents->item, OBJ_COMMIT, data, options);
 		if (result < 0)
 			return result;
@@ -356,8 +424,12 @@ static int fsck_walk_commit(struct commit *commit, void *data, struct fsck_optio
 
 static int fsck_walk_tag(struct tag *tag, void *data, struct fsck_options *options)
 {
+	char *name = get_object_name(options, &tag->object);
+
 	if (parse_tag(tag))
 		return -1;
+	if (name)
+		put_object_name(options, tag->tagged, "%s", name);
 	return options->walk(tag->tagged, OBJ_ANY, data, options);
 }
 
