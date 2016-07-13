6a209fb5df18b4be1315314f0100ed8ca64c0e4c
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-07-13 14:23:51 +0200

    Introduce a new "printf format" for timestamps
    Currently, Git's source code treats all timestamps as if they were
    unsigned longs. Therefore, it is okay to write "%lu" when printing them.
    
    There is a substantial problem with that, though: at least on Windows,
    time_t is *larger* than unsigned long, and hence we will want to switch
    away from the ill-specified `unsigned long` data type.
    
    So let's introduce the pseudo format "PRItime" (currently simply being
    defined to "lu") to make it easier to change the data type used for
    timestamps.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/rev-parse.c
+++ builtin/rev-parse.c
@@ -218,7 +218,8 @@ static void show_datestring(const char *flag, const char *datestr)
 	/* date handling requires both flags and revs */
 	if ((filter & (DO_FLAGS | DO_REVS)) != (DO_FLAGS | DO_REVS))
 		return;
-	snprintf(buffer, sizeof(buffer), "%s%lu", flag, approxidate(datestr));
+	snprintf(buffer, sizeof(buffer), "%s%"PRItime,
+		 flag, approxidate(datestr));
 	show(buffer);
 }
 
