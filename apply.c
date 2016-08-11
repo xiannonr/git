07cfc628d2773f0fc5f353cfe47c5b6067854ba3
Christian Couder <christian.couder@gmail.com>
2016-08-11 20:45:00 +0200

    apply: learn to use a different index file
    Sometimes we want to apply in a different index file.
    
    Before the apply functionality was libified it was possible to
    use the GIT_INDEX_FILE environment variable, for this purpose.
    
    But now, as the apply functionality has been libified, it should
    be possible to do that in a libified way.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- apply.c
+++ apply.c
@@ -4674,8 +4674,14 @@ static int apply_patch(struct apply_state *state,
 		state->apply = 0;
 
 	state->update_index = state->check_index && state->apply;
-	if (state->update_index && state->newfd < 0)
-		state->newfd = hold_locked_index(state->lock_file, 1);
+	if (state->update_index && state->newfd < 0) {
+		if (state->index_file)
+			state->newfd = hold_lock_file_for_update(state->lock_file,
+								 state->index_file,
+								 LOCK_DIE_ON_ERROR);
+		else
+			state->newfd = hold_locked_index(state->lock_file, 1);
+	}
 
 	if (state->check_index && read_cache() < 0) {
 		error(_("unable to read index file"));
