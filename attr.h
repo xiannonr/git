dcf681182a78c91a864d0407d3ae5605a6b86875
Junio C Hamano <gitster@pobox.com>
2016-10-28 11:54:43 -0700

    attr: expose validity check for attribute names
    Export attr_name_valid() function, and a helper function that
    returns the message to be given when a given <name, len> pair
    is not a good name for an attribute.
    
    We could later update the message to exactly spell out what the
    rules for a good attribute name are, etc.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- attr.h
+++ attr.h
@@ -13,6 +13,16 @@ extern struct git_attr *git_attr(const char *);
 /* The same, but with counted string */
 extern struct git_attr *git_attr_counted(const char *, size_t);
 
+/*
+ * Return the name of the attribute represented by the argument.  The
+ * return value is a pointer to a null-delimited string that is part
+ * of the internal data structure; it should not be modified or freed.
+ */
+extern const char *git_attr_name(const struct git_attr *);
+
+extern int attr_name_valid(const char *name, size_t namelen);
+extern void invalid_attr_name_message(struct strbuf *, const char *, int);
+
 /* Internal use */
 extern const char git_attr__true[];
 extern const char git_attr__false[];
