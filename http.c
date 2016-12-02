72334ff7680f708ce980b1aa76a2fe4e55cbf8ce
Brandon Williams <bmwill@google.com>
2016-12-01 16:01:00 -0800

    http: always warn if libcurl version is too old
    Now that there are default "known-good" and "known-bad" protocols which
    are allowed/disallowed by 'is_transport_allowed' we should always warn
    the user that older versions of libcurl can't respect the allowed
    protocols for redirects.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http.c
+++ http.c
@@ -698,9 +698,8 @@ static CURL *get_curl_handle(void)
 		allowed_protocols |= CURLPROTO_FTPS;
 	curl_easy_setopt(result, CURLOPT_REDIR_PROTOCOLS, allowed_protocols);
 #else
-	if (transport_restrict_protocols())
-		warning("protocol restrictions not applied to curl redirects because\n"
-			"your curl version is too old (>= 7.19.4)");
+	warning("protocol restrictions not applied to curl redirects because\n"
+		"your curl version is too old (>= 7.19.4)");
 #endif
 	if (getenv("GIT_CURL_VERBOSE"))
 		curl_easy_setopt(result, CURLOPT_VERBOSE, 1L);
