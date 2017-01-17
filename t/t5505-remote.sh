a36e2c7c5246be8ad007b202cc0df2deb189af52
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-01-17 16:59:30 +0100

    Be more careful when determining whether a remote was configured
    One of the really nice features of the ~/.gitconfig file is that users
    can override defaults by their own preferred settings for all of their
    repositories.
    
    One such default that some users like to override is whether the
    "origin" remote gets auto-pruned or not. The user would simply call
    
    	git config --global remote.origin.prune true
    
    and from now on all "origin" remotes would be pruned automatically when
    fetching into the local repository.
    
    There is just one catch: now Git thinks that the "origin" remote is
    configured, as it does not discern between having a remote whose
    fetch (and/or push) URL and refspec is set, and merely having
    preemptively-configured, global flags for specific remotes.
    
    Let's fix this by telling Git that a remote is not configured unless any
    fetch/push URL or refspect is configured explicitly.
    
    As a special exception, we deem a remote configured also when *only* the
    "vcs" setting is configured. The commit a31eeae27f (remote: use
    remote_is_configured() for add and rename, 2016-02-16) specifically
    extended our test suite to verify this, so it is safe to assume that there
    has been at least one user with a legitimate use case for this.
    
    This fixes https://github.com/git-for-windows/git/issues/888
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5505-remote.sh
+++ t/t5505-remote.sh
@@ -764,7 +764,7 @@ test_expect_success 'rename a remote with name prefix of other remote' '
 	)
 '
 
-test_expect_failure 'rename succeeds with existing remote.<target>.prune' '
+test_expect_success 'rename succeeds with existing remote.<target>.prune' '
 	git clone one four.four &&
 	(
 		cd four.four &&
