ea46c6501e2a180af057257e97827934c968ef4c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-05-11 09:18:05 +0000

    grep: add a test asserting that --perl-regexp dies when !PCRE
    Add a test asserting that when --perl-regexp (and -P for grep) is
    given to git-grep & git-log that we die with an error.
    
    In developing the PCRE v2 series I introduced a regression where -P
    would (through control-flow fall-through) become synonymous with basic
    POSIX matching. I.e. 'git grep -P '[\d]' would match "d" instead of
    digits.
    
    The entire test suite would still pass with this serious regression,
    since everything that tested for --perl-regexp would be guarded by the
    PCRE prerequisite, fix that blind-spot by adding tests under !PCRE
    asserting that git must die when given --perl-regexp or -P.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4202-log.sh
+++ t/t4202-log.sh
@@ -344,6 +344,9 @@ test_expect_success 'log with various grep.patternType configurations & command-
 		then
 			git log --pretty=tformat:%s --perl-regexp \
 				--grep="[\d]\|" >actual.perl.long-arg
+		else
+			test_must_fail git log --perl-regexp \
+				--grep="[\d]\|"
 		fi &&
 		test_cmp expect.fixed actual.fixed.long-arg &&
 		test_cmp expect.basic actual.basic.long-arg &&
