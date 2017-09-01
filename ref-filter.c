f615fcc05fa358b4c7e3531cbdbd91661be321aa
J Wyman <jwyman@microsoft.com>
2017-09-01 16:34:25 -0400

    for-each-ref: let upstream/push optionally report merge name.
    There are times when scripts want to know not only the name of the
    push branch on the remote, but also the name of the branch as known
    by the remote repository.
    
    A useful example of this is with the push command where
    `branch.<name>.merge` is useful as the <to> value. Example:
    
    	$ git push <remote> <from>:<to>
    
    This patch offers the new suffix :merge for the push atom, allowing to
    show exactly that. Example:
    
    	$ cat .git/config
    	...
    	[remote "origin"]
    		url = https://where.do.we.come/from
    		fetch = refs/heads/*:refs/remote/origin/*
    	[branch "master"]
    		remote = origin
    		merge = refs/heads/master
    	[branch "develop/with/topics"]
    		remote = origin
    		merge = refs/heads/develop/with/topics
    	...
    
    	$ git for-each-ref \
    		--format='%(push) %(push:remote-ref)' \
    		refs/heads
    	refs/remotes/origin/master refs/heads/master
    	refs/remotes/origin/develop/with/topics refs/heads/develop/with/topics

--- ref-filter.c
+++ ref-filter.c
@@ -77,7 +77,7 @@ static struct used_atom {
 		struct align align;
 		struct {
 			enum {
-				RR_REF, RR_TRACK, RR_TRACKSHORT, RR_REMOTE_NAME
+				RR_REF, RR_TRACK, RR_TRACKSHORT, RR_REMOTE_NAME, RR_REMOTE_REF
 			} option;
 			struct refname_atom refname;
 			unsigned int nobracket : 1;
@@ -160,6 +160,8 @@ static void remote_ref_atom_parser(const struct ref_format *format, struct used_
 			atom->u.remote_ref.nobracket = 1;
 		else if (!strcmp(s, "remote-name"))
 			atom->u.remote_ref.option = RR_REMOTE_NAME;
+		else if (!strcmp(s, "remote-ref"))
+			atom->u.remote_ref.option = RR_REMOTE_REF;
 		else {
 			atom->u.remote_ref.option = RR_REF;
 			refname_atom_parser_internal(&atom->u.remote_ref.refname,
@@ -1258,6 +1260,14 @@ static void fill_remote_ref_details(struct used_atom *atom, const char *refname,
 			remote_for_branch(branch, &explicit);
 		if (explicit)
 			*s = xstrdup(remote);
+		else
+			*s = "";
+	} else if (atom->u.remote_ref.option == RR_REMOTE_REF) {
+		int explicit, for_push = starts_with(atom->name, "push");
+		const char *merge = remote_ref_for_branch(branch, for_push,
+							  &explicit);
+		if (explicit)
+			*s = xstrdup(merge);
 		else
 			*s = "";
 	} else
