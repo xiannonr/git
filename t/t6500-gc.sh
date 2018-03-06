9266247bdb2738e216981f8e5d56005df9a2ae2f
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2018-03-06 17:41:56 +0700

    gc --auto: exclude base pack if not enough mem to "repack -ad"
    pack-objects could be a big memory hog especially on large repos,
    everybody knows that. The suggestion to stick a .keep file on the
    giant base pack to avoid this problem is also known for a long time.
    
    Let's do the suggestion automatically instead of waiting for people to
    come to Git mailing list and get the advice. When a certain condition
    is met, "gc --auto" tells "git repack" to keep the base pack around.
    The end result would be two packs instead of one.
    
    On linux-2.6.git, valgrind massif reports 1.6GB heap in "pack all"
    case, and 535MB [1] in "pack all except the base pack" case. We save
    roughly 1GB memory by excluding the base pack.
    
    gc --auto decides to do this based on an estimation of pack-objects
    memory usage, which is quite accurate at least for the heap part, and
    whether that fits in half of system memory (the assumption here is for
    desktop environment where there are many other applications running).
    
    Since the estimation may be inaccurate and that 1/2 threshold is
    really arbitrary, give the user a finer control over this mechanism:
    if the largest pack is larger than gc.bigBasePackThreshold, it's kept.
    
    PS. A big chunk of the remaining 535MB is the result of pack-objects
    running rev-list internally. This will be dealt with when we could run
    rev-list externally. Right now we can't because pack-objects internal
    rev-list does more regarding unreachable objects, which cannot be done
    by "git rev-list".
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t6500-gc.sh
+++ t/t6500-gc.sh
@@ -5,6 +5,13 @@ test_description='basic git gc tests
 
 . ./test-lib.sh
 
+test_expect_success 'setup' '
+	# do not let the amount of physical memory affects gc
+	# behavior, make sure the pack_objects_uses_too_much_memory()
+	# always returns false
+	git config gc.bigBasePackThreshold 2g
+'
+
 test_expect_success 'gc empty repository' '
 	git gc
 '
@@ -114,6 +121,28 @@ test_expect_success 'background auto gc respects lock for all operations' '
 	# our gc should exit zero without doing anything
 	run_and_wait_for_auto_gc &&
 	test_path_is_file .git/refs/heads/should-be-loose
+'
+
+test_expect_success 'gc --keep-base-pack' '
+	test_create_repo keep-pack &&
+	(
+		cd keep-pack &&
+		for i in 10; do
+			test_commit $i
+		done &&
+		git gc &&
+		( cd .git/objects/pack && ls *.pack ) >pack-list &&
+		test_line_count = 1 pack-list &&
+		BASE_PACK=.git/objects/pack/pack-*.pack &&
+		for i in 10; do
+			test_commit more-$i
+		done &&
+		git gc --keep-base-pack &&
+		( cd .git/objects/pack && ls *.pack ) >pack-list &&
+		test_line_count = 2 pack-list &&
+		test_path_is_file $BASE_PACK &&
+		git fsck
+	)
 '
 
 # DO NOT leave a detached auto gc process running near the end of the
