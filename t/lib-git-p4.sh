f262ed4afed138fe8d857703407df611343be42d
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-08-13 09:50:25 +0200

    wip

--- t/lib-git-p4.sh
+++ t/lib-git-p4.sh
@@ -202,6 +202,7 @@ kill_p4d () {
 	p4 monitor terminate $pid && return
 	echo "UID: $UID"
 	p4 admin stop && {
+		wait "$pid"
 		ps ax | grep "$pid"
 		ps ax | grep p4
 		#retry_until_fail kill -9 $watchdog_pid
